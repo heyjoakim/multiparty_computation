eda9204962e1a01dfbf7e5151aa3185bacf43649
heyjoakim <joakimhey@gmail.com>
2020-10-12 20:43:16 +0200

    sc

--- client.py
+++ client.py
@@ -1,9 +1,21 @@
-"""" Client """
+"""" Client (localhost) """
 import socket
-host = "127.0.0.1"
-port = 6677
 
+# TCP with ipv4
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect((host, port))
-msg = server.recv(1024)
-print(msg.decode("utf-8"))
+
+host = "127.0.0.1"; port = 6677
+address = (host, port)
+
+server.connect(address)
+full_msg = ''
+
+message = input('> ')
+
+while message != 'quit':
+    msg = server.recv(1)
+    if len(msg) <= 0:
+        break
+    full_msg += msg.decode("utf-8")
+
+print(full_msg)
--- server.py
+++ server.py
@@ -4,15 +4,18 @@ import socket
 # TCP socket with ipv4
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = "127.0.0.1" # faster than localhost due to no DNS lookup
-port = 6677
-server.bind((host,port))
+host = "127.0.0.1"; port = 6677
+address = (host, port)
+server.bind(address)
 
 # Handle connections
-server.listen(2)
+server.listen(10)
+print("[Server started]", "at", host, "on port", port)
 
 while True:
-    clientsocket, address = server.accept() # : -> (conn,adress)
+    client_socket, address = server.accept()
     print(f"Connection from {address} has been established.")
-    clientsocket.send(bytes("Hey client", "utf-8"))
-    clientsocket.close()
+    client_socket.send(bytes("Hey client", "utf-8"))
+    client_socket.close()
+
+
